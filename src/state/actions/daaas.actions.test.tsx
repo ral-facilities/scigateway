c14280ee6ec4198527d5120fe0d304068df65612
Conor <conor.proud@tessella.com>
2019-03-21 16:15:25 +0000

    tests added

--- src/state/actions/daaas.actions.test.tsx
+++ src/state/actions/daaas.actions.test.tsx
@@ -1,5 +1,7 @@
-import { daaasNotification } from './daaas.actions';
+import mockAxios from 'axios';
+import { daaasNotification, configureSite } from './daaas.actions';
 import { NotificationType } from '../daaas.types';
+import { Action } from 'redux';
 
 describe('daaas actions', () => {
   it('daaasNotification should have a message', () => {
@@ -7,5 +9,22 @@ describe('daaas actions', () => {
 
     expect(action.type).toEqual(NotificationType);
     expect(action.payload.message).toEqual('test message');
+  });
+  it('configureSite should dispatch a daaasNotification', async () => {
+    (mockAxios.get as jest.Mock).mockImplementationOnce(() =>
+      Promise.resolve({
+        data: {
+          title: 'new title',
+        },
+      })
+    );
+
+    const asyncAction = configureSite();
+    const actions: Action[] = [];
+    const dispatch = (action: Action): number => actions.push(action);
+
+    await asyncAction(dispatch);
+
+    expect(actions[0]).toEqual(daaasNotification('{"title":"new title"}'));
   });
 });
