4a19effb5a48e59cc5942fa3553976a5385363fb
Ray Millward <Raymond.Millward@tessella.com>
2019-03-27 11:14:28 +0000

    Added e2e plugin for e2e tests

--- server/e2e-test-server.js
+++ server/e2e-test-server.js
@@ -4,7 +4,14 @@ var serveStatic = require('serve-static')
 
 var app = express()
 
+app.get('/settings.json', function (req, res) {
+  console.log('getting settings')
+  res.sendFile(path.join(__dirname, 'e2e-settings.json'));
+});
+
 app.use(serveStatic(path.resolve('./build'), {'index': ['index.html', 'index.htm']}))
+
+
 
 app.get('/*', function (req, res) {
   res.sendFile(path.resolve('./build/index.html'));
--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  public render(): React.ReactNode {
+  public render() {
     return (
       <div className="App">
         <header className="App-header">
