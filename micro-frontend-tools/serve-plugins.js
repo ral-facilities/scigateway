135fd622ab897dac2ec1f206d98a827150403e20
Nick Battam <nick.battam@tessella.com>
2019-04-02 16:41:59 +0100

    Fix code layout issues with serve-plugins

--- micro-frontend-tools/serve-plugins.js
+++ micro-frontend-tools/serve-plugins.js
@@ -2,35 +2,35 @@ var fs = require("fs");
 const path = require('path');
 var exec = require('child_process').exec;
 
-const settingsFilePath = path.join(__dirname, "/dev-plugin-settings.json")
+const settingsFilePath = path.join(__dirname, '/dev-plugin-settings.json');
 
 function checkForSettingsFile() {
-  
   if (!fs.existsSync(settingsFilePath)) {
-    const noSettingsFileMessage = "\n======================================\n" +
-                                  "NO SETTINGS FILE FOUND at\n" +
-                                  `${settingsFilePath}\n` +
-                                  "======================================\n" +
-                                  "There must be a settings file with the location of plugins to \n" +
-                                  "load into the app (which may simply be an empty array).\n\n" +
-                                  "dev-plugin-settings.example.json next to this script can be used as a template\n" + 
-                                  "and renamed as dev-plugin-settings.json in the same folder.\n\n"
-    console.log(noSettingsFileMessage)
-    process.exit(1)
+    const noSettingsFileMessage =
+      '\n======================================\n' +
+      'NO SETTINGS FILE FOUND at\n' +
+      `${settingsFilePath}\n` +
+      '======================================\n' +
+      'There must be a settings file with the location of plugins to \n' +
+      'load into the app (which may simply be an empty array).\n\n' +
+      'dev-plugin-settings.example.json next to this script can be used as a template\n' +
+      'and renamed as dev-plugin-settings.json in the same folder.\n\n';
+    console.log(noSettingsFileMessage);
+    process.exit(1);
   }
 }
 
 function loadPluginsFromSettings() {
-  var content = fs.readFileSync(settingsFilePath);
-  var settings = JSON.parse(content)
-  
-  var plugins = settings.plugins
-  return plugins
+  const content = fs.readFileSync(settingsFilePath);
+  const settings = JSON.parse(content);
+
+  const plugins = settings.plugins;
+  return plugins;
 }
 
 function servePluginWithOutput(plugin, index) {
-  console.log(`Serving plugin ${plugin.location} on port ${plugin.port}`)
-  var child = exec(`serve -l ${plugin.port} ${plugin.location}`);
+  console.log(`Serving plugin ${plugin.location} on port ${plugin.port}`);
+  const child = exec(`serve -l ${plugin.port} ${plugin.location}`);
   child.stdout.on('data', function(data) {
     process.stdout.write(`stdout (${index}): ` + data);
   });
@@ -46,5 +46,5 @@ checkForSettingsFile();
 const plugins = loadPluginsFromSettings();
 
 plugins.forEach((plugin, index) => {
-  servePluginWithOutput(plugin, index)
+  servePluginWithOutput(plugin, index);
 });
