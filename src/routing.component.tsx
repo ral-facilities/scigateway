aee89dfcdb1f49f79beba8cb86090314ff2e6e25
Nick Battam <nick.battam@tessella.com>
2019-04-02 16:40:36 +0100

    Refactor main App code out of index

---
+++ src/routing.component.tsx
@@ -0,0 +1,40 @@
+import * as React from 'react';
+import { Route, Switch } from 'react-router';
+import { StateType } from './state/state.types';
+import { RegisterRoutePayload } from './state/daaas.types';
+import { connect } from 'react-redux';
+
+interface RoutingProps {
+  plugins: RegisterRoutePayload[];
+  location: string;
+}
+
+class Routing extends React.Component<RoutingProps> {
+  public render(): React.ReactNode {
+    const { plugins } = this.props;
+
+    return (
+      <Switch>
+        {plugins.map(p => {
+          console.log(`Adding Route: ${p.link} ${p.displayName}`);
+          return (
+            <Route
+              key={`${p.section}_${p.link}`}
+              path={p.link}
+              render={() => <div id={p.plugin}>{p.displayName}</div>}
+            />
+          );
+        })}
+        <Route exact path="/" render={() => <div>Match</div>} />
+        <Route render={() => <div>Miss</div>} />
+      </Switch>
+    );
+  }
+}
+
+const mapStateToProps = (state: StateType): RoutingProps => ({
+  plugins: state.daaas.plugins,
+  location: state.router.location.pathname,
+});
+
+export default connect(mapStateToProps)(Routing);
