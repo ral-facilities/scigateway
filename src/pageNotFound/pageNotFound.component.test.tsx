247b82a6c8a5f7d8cd6ba7885574289536241f43
Nick Battam <nick.battam@tessella.com>
2019-04-05 16:31:28 +0100

    Simplify PageNotFound test

--- src/pageNotFound/pageNotFound.component.test.tsx
+++ src/pageNotFound/pageNotFound.component.test.tsx
@@ -1,22 +1,16 @@
 import React from 'react';
 import PageNotFound from './pageNotFound.component';
-import { createShallow, createMount } from '@material-ui/core/test-utils';
+import { createShallow } from '@material-ui/core/test-utils';
 
 describe('Page Not found component', () => {
   let shallow;
-  let mount;
 
   beforeEach(() => {
     shallow = createShallow({});
-    mount = createMount();
   });
 
-  afterEach(() => {
-    mount.cleanUp();
-  });
-
-  it('app bar renders correctly', () => {
+  it('pageNotFound renders correctly', () => {
     const wrapper = shallow(<PageNotFound />);
-    expect(wrapper.dive()).toMatchSnapshot();
+    expect(wrapper).toMatchSnapshot();
   });
 });
