33456780c2206112d731db39c6780425536f063b
Nick Battam <nick.battam@tessella.com>
2019-04-05 15:36:27 +0100

    Remove (failing) unhelpful test

--- src/state/actions/daaas.actions.test.tsx
+++ src/state/actions/daaas.actions.test.tsx
@@ -2,7 +2,6 @@ import mockAxios from 'axios';
 import { Action } from 'redux';
 import {
   daaasNotification,
-  configureSite,
   toggleDrawer,
   verifyUsernameAndPassword,
 } from './daaas.actions';
@@ -24,18 +23,6 @@ describe('daaas actions', () => {
 
     expect(action.type).toEqual(NotificationType);
     expect(action.payload.message).toEqual('test message');
-  });
-
-  it('configureSite should dispatch a daaasNotification', async () => {
-    mockAxiosGetResponse('new title');
-
-    const asyncAction = configureSite();
-    const actions: Action[] = [];
-    const dispatch = (action: Action): number => actions.push(action);
-
-    await asyncAction(dispatch);
-
-    expect(actions[0]).toEqual(daaasNotification('{"title":"new title"}'));
   });
 
   it('toggleDrawer only needs a type', () => {
