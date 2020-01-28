0ec89e66858009af8c96bb9b1a4e51f69659a68b
Louise Davies <louise.davies@stfc.ac.uk>
2020-01-28 14:33:11 +0000

    #130 - add refresh handling to middleware so that plugins can request refresh of token

--- src/App.tsx
+++ src/App.tsx
@@ -47,7 +47,8 @@ const store = createStore(
   composeEnhancers(applyMiddleware(...middleware))
 );
 
-listenToPlugins(store.dispatch);
+const getState = store.getState as () => StateType;
+listenToPlugins(store.dispatch, getState);
 
 const dispatch = store.dispatch as ThunkDispatch<StateType, null, AnyAction>;
 dispatch(configureSite());
