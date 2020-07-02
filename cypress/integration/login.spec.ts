5cb211271386b11e70842ee341496e6dec080ab4
tswinb <18294786+tswinb@users.noreply.github.com>
2020-07-02 15:57:05 +0100

    Fix authUrl in auth selector test #211

--- cypress/integration/login.spec.ts
+++ cypress/integration/login.spec.ts
@@ -157,7 +157,7 @@ describe('Login', () => {
         plugins: [],
         'ui-strings': 'res/default.json',
         'auth-provider': 'icat',
-        authUrl: 'http://scigateway-preprod.esc.rl.ac.uk:8000',
+        authUrl: 'http://localhost:8000',
         'help-tour-steps': [],
       });
       cy.route('/authenticators', [
