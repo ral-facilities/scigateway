d5f9c4527a8878b524fa9c606e10c5c76504d4eb
Viktor Bozhinov <viktor.bozhinov@stfc.ac.uk>
2021-01-08 16:20:36 +0000

    Create Contact page component #527

---
+++ src/contactPage/contactPage.component.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { AppStrings } from '../state/scigateway.types';
+
+interface ContactPageProps {
+  res: AppStrings | undefined;
+}
+
+const ContactPage = (): React.ReactElement => {
+  return <div></div>;
+};
+
+export const ContactPageWithoutStyles = ContactPage;
