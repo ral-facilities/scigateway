dab37e9167bb154764f5d2c5defac759668638ea
Viktor Bozhinov <viktor.bozhinov@stfc.ac.uk>
2021-01-11 12:10:41 +0000

    Update ContactPage component unit test accordingly #527

--- src/contactPage/contactPage.component.test.tsx
+++ src/contactPage/contactPage.component.test.tsx
@@ -1,30 +1,30 @@
 import React from 'react';
 import { createShallow } from '@material-ui/core/test-utils';
-import ContactPage from './contactPage.component';
-import configureStore from 'redux-mock-store';
-import { StateType } from '../state/state.types';
-import { initialState } from '../state/reducers/scigateway.reducer';
-import { createLocation } from 'history';
+import {
+  ContactPageWithoutStyles,
+  CombinedContactPageProps,
+} from './contactPage.component';
 
 describe('Contact page componet', () => {
   let shallow;
-  let mockStore;
-  let state: StateType;
+  let props: CombinedContactPageProps;
 
   beforeEach(() => {
     shallow = createShallow({ untilSelector: 'ContactPage' });
 
-    mockStore = configureStore();
-    state = JSON.parse(
-      JSON.stringify({
-        scigateway: initialState,
-        router: { location: createLocation('/contact') },
-      })
-    );
+    props = {
+      res: undefined,
+      classes: {
+        root: 'root-class',
+        container: 'container-class',
+        titleText: 'titleText-class',
+        contactDetails: 'contactDetails-class',
+      },
+    };
   });
 
   it('should render correctly', () => {
-    const wrapper = shallow(<ContactPage store={mockStore(state)} />);
+    const wrapper = shallow(<ContactPageWithoutStyles {...props} />);
     expect(wrapper).toMatchSnapshot();
   });
 });
