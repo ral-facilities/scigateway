adb109470c025af4ee3f94ea02f5b44867551906
Viktor Bozhinov <viktor.bozhinov@stfc.ac.uk>
2021-01-12 14:47:53 +0000

    Create Help page component #478

---
+++ src/helpPage/helpPage.component.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { AppStrings } from '../state/scigateway.types';
+
+interface HelpPageProps {
+  res: AppStrings | undefined;
+}
+
+const HelpPage = (): React.ReactElement => {
+  return <div></div>;
+};
+
+export const HelpPageWithoutStyles = HelpPage;
