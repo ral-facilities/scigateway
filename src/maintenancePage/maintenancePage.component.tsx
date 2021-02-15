60c0ca5b355ee31595ecb5fad2b1ab7cafee87ff
Viktor Bozhinov <viktor.bozhinov@stfc.ac.uk>
2021-02-15 18:30:13 +0000

    Add page content #515

--- src/maintenancePage/maintenancePage.component.tsx
+++ src/maintenancePage/maintenancePage.component.tsx
@@ -1,7 +1,36 @@
 import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import { WithStyles, withStyles } from '@material-ui/core';
+import { connect } from 'react-redux';
+import { StateType } from '../state/state.types';
 
-const MaintenancePage = (): React.ReactElement => {
-  return <div></div>;
+interface MaintenancePageProps {
+  message: string;
+}
+
+export type CombinedMaintenancePageProps = MaintenancePageProps &
+  WithStyles<typeof styles>;
+
+const MaintenancePage = (
+  props: CombinedMaintenancePageProps
+): React.ReactElement => {
+  return (
+    <div className={props.classes.root}>
+      <Typography variant="h3" className={props.classes.titleText}>
+        Maintenance
+      </Typography>
+      <div className={props.classes.container}>
+        <Typography variant="h4">{props.message}</Typography>
+      </div>
+    </div>
+  );
 };
 
+const mapStateToProps = (state: StateType): MaintenancePageProps => ({
+  message: state.scigateway.maintenance.message,
+});
+
 export const MaintenancePageWithoutStyles = MaintenancePage;
+export const MaintenancePageWithStyles = withStyles(styles)(MaintenancePage);
+
+export default connect(mapStateToProps)(MaintenancePageWithStyles);
