ef7dac7e5425f82fdb55aaf40cd43bb45ff6f0f9
Louise Davies <louise.davies@stfc.ac.uk>
2021-11-05 09:58:36 +0000

    #799 - change id of base div from root to scigateway

--- src/index.tsx
+++ src/index.tsx
@@ -7,4 +7,4 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import 'typeface-roboto';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('scigateway'));
