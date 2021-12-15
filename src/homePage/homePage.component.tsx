36346db4e42e79b0b846425ec8f77bb557a3e0af
Joel Davies <joel.davies@stfc.ac.uk>
2021-12-15 10:28:07 +0000

    Adjust homepage headings

--- src/homePage/homePage.component.tsx
+++ src/homePage/homePage.component.tsx
@@ -97,8 +97,16 @@ const useStyles = (props: HomePageProps) => {
         avatarIcon: {
           transform: 'scale(1.75)',
         },
+        paperMainHeading: {
+          fontWeight: 'bold',
+          fontSize: '32px',
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          color: (theme as any).colours?.homePage?.heading,
+          marginBottom: theme.spacing(2),
+        },
         paperHeading: {
           fontWeight: 'bold',
+          fontSize: '24px',
           // eslint-disable-next-line @typescript-eslint/no-explicit-any
           color: (theme as any).colours?.homePage?.heading,
           marginBottom: theme.spacing(2),
@@ -111,6 +119,7 @@ const useStyles = (props: HomePageProps) => {
         },
         bluePaperHeading: {
           fontWeight: 'bold',
+          fontSize: '24px',
           color: '#FFFFFF',
           marginBottom: theme.spacing(2),
         },
@@ -214,7 +223,7 @@ const HomePage = (): React.ReactElement => {
           <Grid container style={{ height: '100%' }}>
             <Grid item xs={6}>
               <Box className={classes.paperContent}>
-                <Typography variant="h4" className={classes.paperHeading}>
+                <Typography variant="h3" className={classes.paperMainHeading}>
                   {t('home-page.browse.title')}
                 </Typography>
                 <Typography
