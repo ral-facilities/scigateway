f13d0f4885c4e6e0f4cdecfcb03292bc71dc17de
Joel Davies <joel.davies@stfc.ac.uk>
2022-01-20 09:39:49 +0000

    Remove storybook #908

--- src/stories/preloader.stories.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Preloader from '../preloader/preloader.component';
-import { storiesOf } from '@storybook/react';
-import { ReduxDecorator } from './utils';
-import '../index.css';
-
-storiesOf('Preloader', module)
-  .addDecorator(ReduxDecorator((s) => s))
-  .add('renders correctly', () => <Preloader />);
