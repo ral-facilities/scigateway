ab3a54bc8185376a865dae0bc5501ff983cc7868
Joel Davies <joel.davies@stfc.ac.uk>
2022-02-09 12:42:27 +0000

    Remove old date picker style overrides (#927)

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,12 +1,1 @@
-import { MuiPickersOverrides } from '@material-ui/pickers/typings/overrides';
-
 declare module 'single-spa';
-
-/* The below provides the ability to override styles for MuiPickersDay */
-type overridesNameToClassKey = {
-  [P in keyof MuiPickersOverrides]: keyof MuiPickersOverrides[P];
-};
-declare module '@mui/material/styles/overrides' {
-  // eslint-disable-next-line @typescript-eslint/no-empty-interface
-  export interface ComponentNameToClassKey extends overridesNameToClassKey {}
-}
