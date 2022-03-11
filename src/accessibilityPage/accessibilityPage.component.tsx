fe8459be72ba4a372befb122f506d097eb19e7f8
joshuadkitenge <joshua.kitenge@stfc.ac.uk>
2022-03-11 14:20:14 +0000

    accessibility page minor tweaks #986

--- src/accessibilityPage/accessibilityPage.component.tsx
+++ src/accessibilityPage/accessibilityPage.component.tsx
@@ -182,10 +182,13 @@ const AccessibiiltyPage = (
         <Typography className={props.classes.description}>
           {t('accessibility-page.contact-us-about-accessibility-text')}
         </Typography>
+
         {/* Here will be Comments box */}
 
         <Typography className={props.classes.description}>
-          {t('accessibility-page.contact-info')}
+          <Link href={`mailto:${t('accessibility-page.contact-info')}`}>
+            {t('accessibility-page.contact-info')}
+          </Link>
         </Typography>
       </div>
 
@@ -320,7 +323,7 @@ const AccessibiiltyPage = (
           >
             This statement was prepared on the{' '}
             <strong>14th of February 2022</strong> It was last reviewed on the{' '}
-            <strong>8th of March 2022</strong>. <br /> This website was last
+            <strong>11th of March 2022</strong>. <br /> This website was last
             tested on <strong> 30th of November 2021 </strong>. The test was
             carried out by the{' '}
             <strong> Data &#38; Software Engineering Group. </strong>
