3bd25fef487599fe5a0e5477c0b95489c9847bde
Kenneth <kenneth.ng.nym@outlook.com>
2023-03-10 12:47:30 +0000

    Migrate preloader tests to RTL

--- src/preloader/preloader.component.test.tsx
+++ src/preloader/preloader.component.test.tsx
@@ -1,16 +1,34 @@
 import React from 'react';
-import Preloader, {
-  Preloader as UnconnectedPreloader,
-} from './preloader.component';
-import configureStore from 'redux-mock-store';
+import Preloader from './preloader.component';
+import configureStore, { MockStore } from 'redux-mock-store';
 import { StateType } from '../state/state.types';
 import { authState, initialState } from '../state/reducers/scigateway.reducer';
 import { createLocation } from 'history';
-import { shallow } from 'enzyme';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { StyledEngineProvider, ThemeProvider } from '@mui/material';
+import { render } from '@testing-library/react';
+import { createTheme } from '@mui/material/styles';
 
 describe('Preloader component', () => {
-  let mockStore;
+  let mockStore: MockStore;
   let state: StateType;
+
+  function Wrapper({
+    children,
+  }: {
+    children: React.ReactElement;
+  }): JSX.Element {
+    return (
+      <Provider store={configureStore([thunk])(state)}>
+        <Provider store={mockStore}>
+          <StyledEngineProvider injectFirst>
+            <ThemeProvider theme={createTheme(false)}>{children}</ThemeProvider>
+          </StyledEngineProvider>
+        </Provider>
+      </Provider>
+    );
+  }
 
   beforeEach(() => {
     state = {
@@ -20,36 +38,29 @@ describe('Preloader component', () => {
         location: createLocation('/'),
       },
     };
-    mockStore = configureStore();
+    mockStore = configureStore()(state);
   });
 
   it('renders fullscreen correctly', () => {
     state.scigateway.siteLoading = true;
 
-    const wrapper = shallow(
-      <Preloader store={mockStore(state)} fullScreen={true} />
-    );
-    expect(wrapper).toMatchSnapshot();
+    const { asFragment } = render(<Preloader fullScreen />, {
+      wrapper: Wrapper,
+    });
+    expect(asFragment()).toMatchSnapshot();
   });
 
   it('renders not fullscreen correctly', () => {
-    const wrapper = shallow(
-      <UnconnectedPreloader loading={true} fullScreen={false} />
-    );
-    expect(wrapper).toMatchSnapshot();
+    const { asFragment } = render(<Preloader fullScreen={false} />, {
+      wrapper: Wrapper,
+    });
+    expect(asFragment()).toMatchSnapshot();
   });
 
-  it('does not render when on the login page', () => {
-    state.router.location = createLocation('/login');
-
-    const wrapper = shallow(<Preloader store={mockStore(state)} />);
-    expect(wrapper).toMatchSnapshot();
-  });
-
-  it('does not render when the site stops loading', () => {
+  it('does not render when loading is false', () => {
     state.scigateway.siteLoading = false;
 
-    const wrapper = shallow(<Preloader store={mockStore(state)} />);
-    expect(wrapper).toMatchSnapshot();
+    const { asFragment } = render(<Preloader />, { wrapper: Wrapper });
+    expect(asFragment()).toMatchSnapshot();
   });
 });
