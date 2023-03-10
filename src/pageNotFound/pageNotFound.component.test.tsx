6fa056b3c419cca2733219dbbefe18a34e166357
Kenneth <kenneth.ng.nym@outlook.com>
2023-03-10 12:46:50 +0000

    Migrate page not found tests to RTL

--- src/pageNotFound/pageNotFound.component.test.tsx
+++ src/pageNotFound/pageNotFound.component.test.tsx
@@ -6,15 +6,30 @@ import { createMemoryHistory, History } from 'history';
 import configureStore from 'redux-mock-store';
 import { StateType } from '../state/state.types';
 import { Provider } from 'react-redux';
-import { ThemeProvider, StyledEngineProvider } from '@mui/material';
+import { StyledEngineProvider, ThemeProvider } from '@mui/material';
 import { buildTheme } from '../theming';
 import { Router } from 'react-router';
-import { mount, shallow } from 'enzyme';
+import { render, screen } from '@testing-library/react';
 
 describe('Page Not found component', () => {
-  let mockStore;
   let state: StateType;
   let history: History;
+
+  function Wrapper({
+    children,
+  }: {
+    children: React.ReactElement;
+  }): JSX.Element {
+    return (
+      <Provider store={configureStore([thunk])(state)}>
+        <Router history={history}>
+          <StyledEngineProvider injectFirst>
+            <ThemeProvider theme={theme}>{children}</ThemeProvider>
+          </StyledEngineProvider>
+        </Router>
+      </Provider>
+    );
+  }
 
   beforeEach(() => {
     mockStore = configureStore([thunk]);
@@ -28,36 +43,17 @@ describe('Page Not found component', () => {
   const theme = buildTheme(false);
 
   it('renders pageNotFound page correctly', () => {
-    const wrapper = shallow(<PageNotFoundComponent />);
-    expect(wrapper).toMatchSnapshot();
-  });
+    const { asFragment } = render(<PageNotFoundComponent />, {
+      wrapper: Wrapper,
+    });
 
-  it('Should have the correct path for the links on the PageNotFound page', () => {
-    const testStore = mockStore(state);
-    const wrapper = mount(
-      <Provider store={testStore}>
-        <Router history={history}>
-          <StyledEngineProvider injectFirst>
-            <ThemeProvider theme={theme}>
-              <PageNotFoundComponent />
-            </ThemeProvider>
-          </StyledEngineProvider>
-        </Router>
-      </Provider>
+    expect(asFragment()).toMatchSnapshot();
+    expect(screen.getByRole('link', { name: 'homepage' })).toHaveAttribute(
+      'href',
+      '/'
     );
-
     expect(
-      wrapper
-        .find('[data-test-id="page-not-found-homepage-link"]')
-        .first()
-        .prop('to')
-    ).toEqual('/');
-
-    expect(
-      wrapper
-        .find('[data-test-id="page-not-found-contact-support-link"]')
-        .first()
-        .prop('to')
-    ).toEqual('footer.links.contact');
+      screen.getByRole('link', { name: 'contact support' })
+    ).toHaveAttribute('href', '/footer.links.contact');
   });
 });
