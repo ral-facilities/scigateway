551c7ff0968b47a4498fd6b1d59daedd9a68663c
Kenneth <kenneth.ng.nym@outlook.com>
2023-03-17 14:34:25 +0000

    Remove enzyme dependencies 🎉

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,8 +1,4 @@
 import '@testing-library/jest-dom';
-import Enzyme from 'enzyme';
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
-// Unofficial React 17 Enzyme adapter
-Enzyme.configure({ adapter: new Adapter() });
 
 function noOp(): void {
   // required as work-around for enzyme/jest environment not implementing window.URL.createObjectURL method
