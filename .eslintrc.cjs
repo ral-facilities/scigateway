4da835be322ad68178c279b62bdc60ee1b4950af
Joel Davies <joel.davies@stfc.ac.uk>
2024-03-19 12:19:17 +0000

    Cleanup eslint config #1379

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,25 +1,39 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
   env: {
     browser: true,
-    jest: true,
     es6: true,
     node: true,
   },
+  settings: {
+    react: {
+      version: 'detect', // Tells eslint-plugin-react to automatically detect the version of React to use
+    },
+  },
+  plugins: [
+    'react',
+    'react-hooks',
+    '@typescript-eslint',
+    'cypress',
+    'prettier',
+  ],
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/stylistic',
-    'prettier',
     'plugin:cypress/recommended',
+    'prettier',
   ],
-  plugins: ['prettier', 'cypress'],
   rules: {
     'react/react-in-jsx-scope': 'off',
-    'react/jsx-filename-extension': 'off',
-    'jsx-a11y/click-events-have-key-events': 'off',
     'prettier/prettier': [
       'error',
       {
@@ -28,8 +42,6 @@ module.exports = {
         endOfLine: 'auto',
       },
     ],
-    // disable for all files - this means we can have plain JS files not causing errors
-    '@typescript-eslint/explicit-function-return-type': 'off',
     // Emulate typescript style for unused variables, see
     // https://typescript-eslint.io/rules/no-unused-vars/
     '@typescript-eslint/no-unused-vars': [
@@ -47,33 +59,8 @@ module.exports = {
   },
   overrides: [
     {
-      // and enable again specifically for TS files
-      files: ['*.ts', '*.tsx'],
-      rules: {
-        '@typescript-eslint/explicit-function-return-type': [
-          'error',
-          {
-            allowExpressions: true,
-            allowTypedFunctionExpressions: true,
-          },
-        ],
-      },
-    },
-    {
       files: ['**/?*test.*'],
       extends: ['plugin:testing-library/react'],
     },
   ],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    allowImportExportEverywhere: true,
-    sourceType: 'module',
-  },
-  settings: {
-    react: {
-      version: 'detect', // Tells eslint-plugin-react to automatically detect the version of React to use
-    },
-  },
 };
